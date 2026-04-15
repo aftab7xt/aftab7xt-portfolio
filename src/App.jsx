45796a055dec197e48082e2a1ad283e36cb59323
aftab7xt <skaftaballi32@gmail.com>
2026-04-15 07:14:20 +0530

    Create App component with a welcome message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Shaikh Aftab <span className="text-blue-500">Alli</span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
